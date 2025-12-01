284775404fc09bdf05f97d38920aafecb051d20f
PadoleKiran <124488404+PadoleKiran@users.noreply.github.com>
2025-12-01 16:08:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let ok = document.getElementById("ok");
+let cancle = document.getElementById("cancel");
+
+function confirm() {
+	alert("You clicked OK. Proceeding...");
+}
+
+function cancel() {
+	alert("You clicked Cancel. Exiting...");
+}
